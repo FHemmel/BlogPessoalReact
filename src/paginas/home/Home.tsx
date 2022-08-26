5dc061f16c1663bbb261e172ab7ee4bfdf8a5f64
Fernando Hemmel <fernando_hs123@outlook.com>
2022-08-25 22:59:52 -0300

    Criando componentes Home e estilizando com CSS

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://img.freepik.com/vetores-gratis/fundo-cinza-geometrico_53876-113908.jpg?w=2000" alt="Teste" className="img"/>
+        </>
+    );
+}
+
+export default Home;
